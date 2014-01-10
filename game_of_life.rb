4b40d5efbc47aa6c6eda80e4b6846edd777efae3
kevivmatrix <vivekpatna52@gmail.com>
2014-01-10 23:26:43 +0530

    dead and alive neighbours

--- game_of_life.rb
+++ game_of_life.rb
@@ -2,29 +2,43 @@ class GameOfLife
 
   attr_accessor :total_cells, :matrix, :cells
 
-  def initialize total_cells=5, matrix_size=10
-    @total_cells, @matrix, @cells = total_cells, Matrix.new(matrix_size), []
+  def initialize matrix_size=10
+    @matrix, @cells = Matrix.new(matrix_size), []
   end
 
   def generate_cells
-    total_cells.times do
-      cells << Cell.new(matrix.get_x_within_matrix, matrix.get_y_within_matrix)
+    matrix.structure.each_with_index do |element, x|
+      element.each_with_index do |element, y|
+        cells << Cell.new(x, y)
+      end
     end
   end
 end
 
 class Cell
 
-  attr_accessor :x, :y
+  attr_accessor :x, :y, :alive
 
-  def initialize x, y
-    @x, @y = x, y
+  def initialize x, y, alive=false
+    @x, @y, @alive = x, y, alive
   end
 
-  def neighbours cells
+  def neighbours_among cells
     cells.select do |cell|
       distance_from(cell) <= 2 && distance_from(cell) > 0
     end
+  end
+
+  def alive_neighbours_among cells
+    neighbours_among(cells).select { |cell| cell.alive }
+  end
+
+  def dead_neighbours_among cells
+    neighbours_among(cells).select { |cell| cell.dead? }
+  end
+
+  def dead?
+    !alive
   end
 
   def distance_from cell
@@ -34,10 +48,13 @@ end
 
 class Matrix
 
-  attr_accessor :size
+  attr_accessor :size, :structure
 
   def initialize size
     @size = size
+    @structure = Array.new(size) { 
+      Array.new(size)
+    }
   end
 
   def get_x_within_matrix
--- game_of_life_spec.rb
+++ game_of_life_spec.rb
@@ -4,23 +4,9 @@ require "./game_of_life.rb"
 
 describe "game of life" do
   context 'initialize game' do
-    context 'cells' do
-      context 'game should start with 5 cells if not provided' do
-        let(:game_of_life) { GameOfLife.new }
-        it "should have 5 cells" do
-          game_of_life.total_cells.should eq 5
-        end
-      end
-      context 'game should start with given cells if provided' do
-        let(:game_of_life) { GameOfLife.new 10 }
-        it "should have 10 cells" do
-          game_of_life.total_cells.should eq 10
-        end
-      end
-    end
     context 'matrix' do
       context 'game should start with matrix of 10 if not provided' do
-        let(:game_of_life) { GameOfLife.new 5 }
+        let(:game_of_life) { GameOfLife.new }
         it "should return a matrix object" do
           game_of_life.matrix.should be_a Matrix
         end
@@ -29,7 +15,7 @@ describe "game of life" do
         end
       end
       context 'game should start with given matrix size if provided' do
-        let(:game_of_life) { GameOfLife.new 5, 25 }
+        let(:game_of_life) { GameOfLife.new 25 }
         it "should create a matrix with size of 25" do
           game_of_life.matrix.size.should eq 25
         end
@@ -38,11 +24,11 @@ describe "game of life" do
   end
 
   context 'start game' do
-    let(:game_of_life) { GameOfLife.new(5, 10) }
+    let(:game_of_life) { GameOfLife.new(5) }
     let(:generate_cells) { game_of_life.generate_cells }
     let(:cells) { game_of_life.cells }
-    it "should create 5 cells" do
-      expect { generate_cells }.to change(cells, :count).by(5)
+    it "should create 25 cells" do
+      expect { generate_cells }.to change(cells, :count).by(25)
     end
     context 'x and y for each cell should be set within limits' do
       before do
@@ -78,16 +64,30 @@ describe "matrix" do
     it "should be less than size of matrix" do
       y.should < 10
     end
+    context 'structure' do
+      it "should have be an array" do
+        matrix.structure.should be_a Array
+      end
+      it "should have 10 elements" do
+        matrix.structure.count.should eq 10
+      end
+      it "each element should be an array" do
+        matrix.structure[0].should be_a Array
+      end
+      it "each element should be an array of 10 sub-elements" do
+        matrix.structure[0].count.should eq 10
+      end
+    end
   end
 end
 
 describe Cell do
-  let(:game_of_life) { GameOfLife.new(6, 10) }
-  let(:cell) { Cell.new(1, 1) }
-  let(:cell1) { Cell.new(1, 2) }
-  let(:cell2) { Cell.new(2, 2) }
-  let(:cell3) { Cell.new(2, 1) }
-  let(:cell4) { Cell.new(1, 0) }
+  let(:game_of_life) { GameOfLife.new(10) }
+  let(:cell) { Cell.new(1, 1, true) }
+  let(:cell1) { Cell.new(1, 2, true) }
+  let(:cell2) { Cell.new(2, 2, true) }
+  let(:cell3) { Cell.new(2, 1, false) }
+  let(:cell4) { Cell.new(1, 0, false) }
   let(:cell5) { Cell.new(3, 2) }
   before do
     game_of_life.cells = [
@@ -96,7 +96,17 @@ describe Cell do
   end
   context 'neighbours' do
     it "should equal cell1, cell2, cell3, cell4" do
-      cell.neighbours(game_of_life.cells).should =~ [cell1, cell2, cell3, cell4]
+      cell.neighbours_among(game_of_life.cells).should =~ [cell1, cell2, cell3, cell4]
+    end
+  end
+  context "alive neighbours" do
+    it "should return alive neighbour cells - cell1, cell2" do
+      cell.alive_neighbours_among(game_of_life.cells).should =~ [cell1, cell2]
+    end
+  end
+  context "dead neighbours" do
+    it "should return dead neighbour cells - cell3, cell4" do
+      cell.dead_neighbours_among(game_of_life.cells).should =~ [cell3, cell4]
     end
   end
 end
